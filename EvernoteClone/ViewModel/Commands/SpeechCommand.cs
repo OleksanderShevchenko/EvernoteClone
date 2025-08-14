ccb07979109382b0d836df356f37121d6abfdb13
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-08-14 17:11:48 +0300

    Add SpeechCommand for speech button

---
+++ EvernoteClone/ViewModel/Commands/SpeechCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EvernoteClone.ViewModel.Commands
+{
+	public class SpeechCommand: ICommand
+	{
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return true; // You can add logic to determine if the command can execute
+		}
+		public void Execute(object parameter)
+		{
+			// Logic to handle speech command execution
+			Console.WriteLine("Speech command executed.");
+		}
+		
+		private NotesVM _notesVM;
+		public SpeechCommand(NotesVM vm)
+		{
+			_notesVM = vm;
+		}
+	}
+
+}
