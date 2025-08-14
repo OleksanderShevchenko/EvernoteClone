06dd98fc42ccac7add9a05cf6e251fc136c40b73
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-08-14 17:07:43 +0300

    Add exit command

---
+++ EvernoteClone/ViewModel/Commands/ExitCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EvernoteClone.ViewModel.Commands
+{
+	public class ExitCommand: ICommand
+	{
+		public event EventHandler CanExecuteChanged;
+		public bool CanExecute(object parameter)
+		{
+			return true; // Logic to determine if the command can execute
+		}
+		public void Execute(object parameter)
+		{
+			// Logic to execute when the command is invoked
+			Console.WriteLine("Exit command executed.");
+			System.Windows.Application.Current.Shutdown();
+		}
+
+		private NotesVM _notesVM;
+
+		public ExitCommand(NotesVM vm)
+		{
+			_notesVM = vm;
+		}
+	}
+}
