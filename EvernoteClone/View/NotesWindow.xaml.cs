c5136fd9a5e1382c81333626cfdf0d5aa050b83f
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-08-19 21:51:55 +0300

    Add new buttons  and comboboxes to toolbar  edit text and save note. Add event methods for them and initialization of combobox's content

--- EvernoteClone/View/NotesWindow.xaml.cs
+++ EvernoteClone/View/NotesWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using EvernoteClone.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,7 @@ namespace EvernoteClone.View
 		private NotesVM _viewModel;
 		public NotesWindow()
 		{
+			InitializeComponent();
 			// Step 1: Create a SINGLE instance of the ViewModel
 			_viewModel = new NotesVM();
 
@@ -32,6 +34,12 @@ namespace EvernoteClone.View
 
 			// Step 3: Listen for changes on the RecognizedText property from this instance
 			_viewModel.PropertyChanged += ViewModel_PropertyChanged;
+
+			var fontFamilies = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+			fontFamilyComboBox.ItemsSource = fontFamilies;
+
+			List<double> fontSizes = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 28, 48, 72 };
+			fontSizeComboBox.ItemsSource = fontSizes;
 		}
 
 		private void ViewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
@@ -76,6 +84,72 @@ namespace EvernoteClone.View
 		{
 			var selectedWeight = rtbNoteContent.Selection.GetPropertyValue(FontWeightProperty);
 			btnBold.IsChecked = (selectedWeight != DependencyProperty.UnsetValue) && (selectedWeight.Equals(FontWeights.Bold));
+
+			var selectedStyle = rtbNoteContent.Selection.GetPropertyValue(Inline.FontStyleProperty);
+			btnItalic.IsChecked = (selectedStyle != DependencyProperty.UnsetValue) && (selectedStyle.Equals(FontStyles.Italic));
+
+			var selecteDecoration = rtbNoteContent.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+			btnUnderlined.IsChecked = (selecteDecoration != DependencyProperty.UnsetValue) && (selecteDecoration.Equals(TextDecorations.Underline));
+
+			fontFamilyComboBox.SelectedItem = rtbNoteContent.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+			string fontSize = rtbNoteContent.Selection.GetPropertyValue(Inline.FontSizeProperty).ToString();
+			if (fontSize != "{DependencyProperty.UnsetValue}")
+				fontSizeComboBox.Text = fontSize;
+			else
+				fontSizeComboBox.Text = string.Empty;
+		}
+
+		private void btnItalic_Click(object sender, RoutedEventArgs e)
+		{
+			bool isItalic = (sender as ToggleButton)?.IsChecked ?? false;
+			if (isItalic)
+			{
+				rtbNoteContent.Selection.ApplyPropertyValue(Inline.FontStyleProperty, FontStyles.Italic);
+			}
+			else
+			{
+				rtbNoteContent.Selection.ApplyPropertyValue(Inline.FontStyleProperty, FontStyles.Normal);
+			}
+		}
+
+		private void btnUnderlined_Click(object sender, RoutedEventArgs e)
+		{
+			bool isButtonEnabled = (sender as ToggleButton).IsChecked ?? false;
+
+			if (isButtonEnabled)
+				rtbNoteContent.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+			else
+			{
+				TextDecorationCollection textDecorations;
+				(rtbNoteContent.Selection.GetPropertyValue(Inline.TextDecorationsProperty) as TextDecorationCollection).TryRemove(TextDecorations.Underline, out textDecorations);
+				rtbNoteContent.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, textDecorations);
+			}
+		}
+
+		private void fontFamilyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (fontFamilyComboBox.SelectedItem != null)
+				rtbNoteContent.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, fontFamilyComboBox.SelectedItem);
+		}
+
+		private void fontSizeComboBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (fontSizeComboBox.Text != string.Empty)
+				rtbNoteContent.Selection.ApplyPropertyValue(Inline.FontSizeProperty, fontSizeComboBox.Text);
+		}
+
+		private void saveFileButton_Click(object sender, RoutedEventArgs e)
+		{
+			string rtfFile = System.IO.Path.Combine(Environment.CurrentDirectory, $"{_viewModel.SelectedNote.Id}.rtf");
+			_viewModel.SelectedNote.FileLocation = rtfFile;
+
+			using (FileStream fileStream = new FileStream(rtfFile, FileMode.Create))
+			{
+				TextRange range = new TextRange(rtbNoteContent.Document.ContentStart, rtbNoteContent.Document.ContentEnd);
+				range.Save(fileStream, DataFormats.Rtf);
+			}
+
+			_viewModel.UpdateSelectedNote();
 		}
 	}
 }
