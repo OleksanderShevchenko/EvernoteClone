ec163954a61a971f56338524ba420a6eab80d129
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-08-19 20:51:15 +0300

    Remove resources - use cs code to define viewmodel. Change speech button to ToggleButton type. Add language combobox. Improve btnBold_Click method by checking ToggleButton state. Add rtbNoteContent_SelectionChanged method to react on selection change in reach text box to properly set btnBold state

--- EvernoteClone/View/NotesWindow.xaml.cs
+++ EvernoteClone/View/NotesWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using EvernoteClone.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -19,9 +21,36 @@ namespace EvernoteClone.View
 	/// </summary>
 	public partial class NotesWindow : Window
 	{
+		private NotesVM _viewModel;
 		public NotesWindow()
 		{
-			InitializeComponent();
+			// Step 1: Create a SINGLE instance of the ViewModel
+			_viewModel = new NotesVM();
+
+			// Step 2: Set the DataContext of the window to this instance
+			this.DataContext = _viewModel;
+
+			// Step 3: Listen for changes on the RecognizedText property from this instance
+			_viewModel.PropertyChanged += ViewModel_PropertyChanged;
+		}
+
+		private void ViewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == nameof(NotesVM.RecognizedText))
+			{
+				// Update the RichTextBox with the new text.
+				// Note: This approach clears and re-populates the text each time.
+				// For a more efficient "append" behavior, you could check if the new text
+				// is a continuation of the old one and only add the new part.
+				rtbNoteContent.Document.Blocks.Clear();
+				rtbNoteContent.Document.Blocks.Add(new Paragraph(new Run(_viewModel.RecognizedText)));
+			}
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			// Call the cleanup method on the ViewModel
+			_viewModel.StopRecognition();
 		}
 
 		private void rtbNoteContent_TextChanged(object sender, TextChangedEventArgs e)
@@ -32,7 +61,8 @@ namespace EvernoteClone.View
 
 		private void btnBold_Click(object sender, RoutedEventArgs e)
 		{
-			if (btnBold.IsChecked == true)
+			bool isBold = (sender as ToggleButton)?.IsChecked ?? false;
+			if (isBold)
 			{
 				rtbNoteContent.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Bold);
 			}
@@ -40,6 +70,12 @@ namespace EvernoteClone.View
 			{
 				rtbNoteContent.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Normal);
 			}
+		}
+
+		private void rtbNoteContent_SelectionChanged(object sender, RoutedEventArgs e)
+		{
+			var selectedWeight = rtbNoteContent.Selection.GetPropertyValue(FontWeightProperty);
+			btnBold.IsChecked = (selectedWeight != DependencyProperty.UnsetValue) && (selectedWeight.Equals(FontWeights.Bold));
 		}
 	}
 }
