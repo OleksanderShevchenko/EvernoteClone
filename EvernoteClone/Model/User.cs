8be6bdd86c1189f70d9a01e2d26730473e0cf00d
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-08-24 16:56:22 +0300

    Update User class to implement INotifyPropertyChanged interface.

--- EvernoteClone/Model/User.cs
+++ EvernoteClone/Model/User.cs
@@ -1,19 +1,90 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace EvernoteClone.Model
 {
-    public class User
-    {
-        [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
-        [MaxLength(50)]
-		public string Name { get; set; }
+    public class User : INotifyPropertyChanged
+	{
+		private int id;
+		[PrimaryKey, AutoIncrement]
+        public int Id 
+		{ 
+			get { return id; }
+			set
+			{
+				id = value;
+				OnPropertyChanged(nameof(Id));
+			}
+		}
+
+		private string name;
 		[MaxLength(50)]
-		public string Lastname { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
+		public string Name
+		{
+			get { return name; }
+			set
+			{
+				name = value;
+				OnPropertyChanged(nameof(Name));
+			} 
+		}
+
+		private string lastname;
+		[MaxLength(50)]
+		public string Lastname 
+		{
+			get { return lastname; }
+			set
+			{
+				lastname = value;
+				OnPropertyChanged(nameof(Lastname));
+			}
+		}
+
+		private string userName;
+		[MaxLength(50)]
+		public string Username 
+		{
+			get { return userName; }
+			set
+			{
+				userName = value;
+				OnPropertyChanged(nameof(Username));
+			}
+		}
+
+		private string email;
+		[MaxLength(150)]
+		public string Email
+		{
+			get { return email; }
+			set
+			{
+				email = value;
+				OnPropertyChanged(nameof(Email));
+			}
+		}
+
+		private string password;
+		[MaxLength(100)]
+		public string Password 
+		{
+			get { return password; }
+			set
+			{
+				password = value;
+				OnPropertyChanged(nameof(Password));
+			}
+		}
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
 }
