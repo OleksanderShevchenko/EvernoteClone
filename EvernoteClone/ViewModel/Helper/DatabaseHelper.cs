73b1c6d8acdc0b88794a5357808d95b8b4eaada9
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-08-24 17:58:57 +0300

    Update DBHelper - use MobileServiceClient class from Microsoft.WindowsAzure.MobileServices to connect to Azure database. Modify methods to use new settings and support both Azure and local SQLite databases

--- EvernoteClone/ViewModel/Helper/DatabaseHelper.cs
+++ EvernoteClone/ViewModel/Helper/DatabaseHelper.cs
@@ -1,4 +1,5 @@
 ﻿using EvernoteClone.Core;
+using Microsoft.WindowsAzure.MobileServices;
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
@@ -13,51 +14,105 @@ namespace EvernoteClone.ViewModel.Helper
 	public class DatabaseHelper
 	{
 		private static string dbName = AppConfig.Instance.DbName;
+		public static MobileServiceClient client = new MobileServiceClient(AppConfig.Instance.MobileClient);
 		private static string dbFile = Path.Combine(Environment.CurrentDirectory, dbName);
+		
+		public static bool UseAzureDB
+		{
+			get
+			{
+				return AppConfig.Instance.UseAzureDB;
+			}
+
+		}
 
 		public static async Task<bool> Insert<T>(T item)
 		{
 			bool result = false;
-
-			using (var db = new SQLite.SQLiteConnection(dbFile))
+			if (UseAzureDB)
 			{
-				db.CreateTable<T>();
-				int rows = db.Insert(item);
-				if (rows > 0)
+				try
 				{
+					await client.GetTable<T>().InsertAsync(item);
 					result = true;
 				}
+				catch (Exception ex)
+				{
+					result = false;
+				}
 			}
+			else  // local database
+			{
+				using (var db = new SQLite.SQLiteConnection(dbFile))
+				{
+					db.CreateTable<T>();
+					int rows = db.Insert(item);
+					if (rows > 0)
+					{
+						result = true;
+					}
+				}
+			}
+				
 			return result;
 		}
 
 		public static async Task<bool> Update<T>(T item)
 		{
 			bool result = false;
-
-			using (var db = new SQLite.SQLiteConnection(dbFile))
+			if (UseAzureDB)
 			{
-				db.CreateTable<T>();
-				int rows = db.Update(item);
-				if (rows > 0)
+				try
 				{
+					await client.GetTable<T>().UpdateAsync(item);
 					result = true;
 				}
+				catch (Exception ex)
+				{
+					result = false;
+				}
 			}
+			else // local database
+			{
+				using (var db = new SQLite.SQLiteConnection(dbFile))
+				{
+					db.CreateTable<T>();
+					int rows = db.Update(item);
+					if (rows > 0)
+					{
+						result = true;
+					}
+				}
+			}
+				
 			return result;
 		}
 
 		public static async Task<bool> Delete<T>(T item)
 		{
 			bool result = false;
-
-			using (var db = new SQLite.SQLiteConnection(dbFile))
+			if (!UseAzureDB)
 			{
-				db.CreateTable<T>();
-				int rows = db.Delete(item);
-				if (rows > 0)
+				try
 				{
+					await client.GetTable<T>().DeleteAsync(item);
 					result = true;
+				}
+				catch (Exception ex)
+				{
+					result = false;
+				}
+			}
+			else // local database
+			{
+				using (var db = new SQLite.SQLiteConnection(dbFile))
+				{
+					db.CreateTable<T>();
+					int rows = db.Delete(item);
+					if (rows > 0)
+					{
+						result = true;
+					}
 				}
 			}
 			return result;
@@ -66,11 +121,25 @@ namespace EvernoteClone.ViewModel.Helper
 		public static async Task<List<T>> Read<T>() where T : new()
 		{
 			List<T> result = new List<T>();
-
-			using (var db = new SQLite.SQLiteConnection(dbFile))
+			if (UseAzureDB)
 			{
-				db.CreateTable<T>();
-				db.Table<T>().ToList().ForEach(item => result.Add(item));
+				try
+				{
+					var items = await client.GetTable<T>().ToListAsync();
+					result.AddRange(items);
+				}
+				catch (Exception ex)
+				{
+					return new List<T>();
+				}
+			}
+			else  // local database
+			{
+				using (var db = new SQLite.SQLiteConnection(dbFile))
+				{
+					db.CreateTable<T>();
+					db.Table<T>().ToList().ForEach(item => result.Add(item));
+				}
 			}
 			return result;
 		}
