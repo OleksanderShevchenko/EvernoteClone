34ba470fc466e73de3d7c8d0114f24558d5c580b
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-08-24 16:39:19 +0300

    Update Notebook class to inplement pattern INotifyPropertyChanged

--- EvernoteClone/Model/Notebook.cs
+++ EvernoteClone/Model/Notebook.cs
@@ -1,18 +1,75 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace EvernoteClone.Model
 {
-    public class Notebook
-    {
+    public class Notebook : INotifyPropertyChanged
+	{
+		private int id;
 		[PrimaryKey, AutoIncrement]
-		public int Id { get; set; }
-        [Indexed]
-        public int UserId { get; set; }
-        public string Name { get; set; }
-		public DateTime CreatedAt { get; set; }
-		public DateTime UpdatedAt { get; set; }
+		public int Id 
+		{
+			get { return id; }
+			set
+			{
+				id = value;
+				OnPropertyChanged(nameof(Id));
+			}
+		}
+
+		private int userId;
+		[Indexed]
+        public int UserId
+		{
+			get { return userId; }
+			set
+			{
+				userId = value;
+				OnPropertyChanged(nameof(UserId));
+			}
+		}
+
+		private string name;
+		public string Name 
+		{ 
+			get { return name; }
+			set
+			{
+				name = value;
+				OnPropertyChanged(nameof(Name));
+			}
+		}
+
+		private DateTime createdAt;
+		public DateTime CreatedAt 
+		{
+			get { return createdAt; }
+			set
+			{
+				createdAt = value;
+				OnPropertyChanged(nameof(CreatedAt));
+			}
+		}
+
+		private DateTime updatedAt;
+		public DateTime UpdatedAt 
+		{
+			get { return updatedAt; }
+			set
+			{
+				updatedAt = value;
+				OnPropertyChanged(nameof(UpdatedAt));
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
 	}
 }
