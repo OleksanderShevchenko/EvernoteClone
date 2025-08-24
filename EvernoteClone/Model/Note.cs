c54a35acc6350358d2e853fa889048255db89bdb
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-08-24 11:11:53 +0300

    Implement INotifyPropertyChanged interface in the model class Note

--- EvernoteClone/Model/Note.cs
+++ EvernoteClone/Model/Note.cs
@@ -1,19 +1,88 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace EvernoteClone.Model
 {
-    public class Note
-    {
+    public class Note : INotifyPropertyChanged
+	{
+		private int id;
 		[PrimaryKey, AutoIncrement]
-		public int Id { get; set; }
-        [Indexed]
-        public int NotebookId { get; set; }
-        public string Title { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-        public string FileLocation { get; set; }
-    }
+		public int Id 
+        { get { return id; }
+          set
+            {
+				id = value;
+                OnPropertyChanged(nameof(Id));
+			}
+        }
+
+        private int notebookId;
+		[Indexed]
+        public int NotebookId 
+        {
+            get { return notebookId; }
+            set
+            {
+				notebookId = value;
+				OnPropertyChanged(nameof(NotebookId));
+			}
+        }
+
+		private string title;
+		public string Title 
+		{
+			get { return title; }
+			set
+			{
+				title = value;
+				OnPropertyChanged(nameof(Title));
+			}
+		}
+
+		private DateTime createdAt;
+		public DateTime CreatedAt 
+		{
+			get { return createdAt; }
+			set
+			{
+				createdAt = value;
+				OnPropertyChanged(nameof(CreatedAt));
+			}
+		}
+
+		private DateTime updatedAt;
+		public DateTime UpdatedAt 
+		{
+			get { return updatedAt; }
+			set
+			{
+				updatedAt = value;
+				OnPropertyChanged(nameof(UpdatedAt));
+			}
+		}
+
+		private string fileLocation;
+        public string FileLocation 
+		{
+			get { return fileLocation; }
+			set
+			{
+				fileLocation = value;
+				OnPropertyChanged(nameof(FileLocation));
+			}
+		}
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
 }
